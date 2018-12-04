27498c6541613929970c8eb2a07961d4b82b2670
Jef Spaleta <jef@sensu.io>
2018-12-03 19:03:05 -0900

    more travis testing

--- travis/generate-sha512sum.sh
+++ travis/generate-sha512sum.sh
@@ -6,9 +6,13 @@ if [ -d dist ]; then
   file=`basename ${files[0]}`
   IFS=\_ read -r package prefix x <<< $file
   if [ -n "$prefix" ]; then
+    export package
+    export prefix
     echo "Generating sha512sum for ${package}_${prefix}"
     cd dist
     sha512sum *.tar.gz > "${package}_${prefix}_sha512-checksum.txt"
-    cat "${package}_${prefix}_sha512-checksum.txt"
+    sha512_file="${package}_${prefix}_sha512-checksum.txt"
+    export sha512_file
+    cat "${sha512}"
   fi
 fi
