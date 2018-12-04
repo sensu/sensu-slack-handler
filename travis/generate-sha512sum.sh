fa3998f1fef0ddfbda3bf46704f05276b612ec59
Jef Spaleta <jef@sensu.io>
2018-12-03 23:16:07 -0900

    use file to hold name of sha512 checksum file

--- travis/generate-sha512sum.sh
+++ travis/generate-sha512sum.sh
@@ -6,14 +6,11 @@ if [ -d dist ]; then
   file=`basename ${files[0]}`
   IFS=\_ read -r package prefix x <<< $file
   if [ -n "$prefix" ]; then
-    export package
-    export prefix
     echo "Generating sha512sum for ${package}_${prefix}"
     cd dist
-    sha512_file="${package}_${prefix}_sha512-checksum.txt"
-    export sha512_file
-    echo "sha512_file: ${sha512_file}"
+    sha512_file="${package}_${prefix}_sha512-checksums.txt"
     echo "${sha512_file}" > sha512_file
+    echo "sha512_file: $(cat sha512_file)"
     sha512sum *.tar.gz > "${sha512_file}"
     echo ""
     cat "${sha512_file}"
